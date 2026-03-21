06296098abb0ba3005c4259ed4dae9695f21cc72
Naresh Khatri <nareshkhatri9810@gmail.com>
2026-03-21 23:33:56 +0545

    update project section with new project details and image

--- src/components/projects/ProjectSection.tsx
+++ src/components/projects/ProjectSection.tsx
@@ -7,10 +7,11 @@ const ProjectSection = () => {
         <Header title="My Projects"/>
         <div className="w-[80%] m-auto grid grid-cols-1 md:grid-cols-2 gap-4 overflow-hidden">
          <div data-aos = "fade-right">
-            <ProjectCard title=" Modern Dashboard UI" imagePath="/images/image.png" description="A modern dashboard UI built with React and Tailwind CSS."/>
+            <ProjectCard title=" Learning Management System" imagePath="/images/image.png" description="An LMS developed using the MERAN algorithm (Map, Explore, Recommend, Analyze, Navigate) personalizes learning by analyzing user behavior and delivering adaptive course recommendations.
+It enhances student engagement and learning outcomes through intelligent content "/>
          </div>
          <div data-aos="fade-left">
-           <ProjectCard title=" Modern Dashboard UI" imagePath="/images/p2.webp" description="A modern dashboard UI built with React and Tailwind CSS."/> 
+           <ProjectCard title=" HR Analytics Dashboard" imagePath="/images/p7.png" description="An HR Analytics Dashboard built using Microsoft Power BI visualizes key HR metrics like employee performance, attrition, and recruitment trends."/> 
          </div>
          <div data-aos="fade-right">
             <ProjectCard title=" Modern Dashboard UI" imagePath="/images/p3.webp" description="A modern dashboard UI built with React and Tailwind CSS."/>
